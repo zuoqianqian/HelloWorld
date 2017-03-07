61f12dce63ba6175e750888a4c61ba53058c83e1
zuoqianqian <1577582192@qq.com>
2017-03-07 18:05:26 +0800

    第一次提交，新建HelloWorld项目

---
+++ src/org/boco/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.boco.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world hahaha");
+		
+	}
+
+}
