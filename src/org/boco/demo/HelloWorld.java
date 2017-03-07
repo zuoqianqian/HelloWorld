1a54a787970f0497cbbd67ac36708c96e60e91c4
zuoqianqian <1577582192@qq.com>
2017-03-07 18:16:09 +0800

    更新一

--- src/org/boco/demo/HelloWorld.java
+++ src/org/boco/demo/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world hahaha");
-		
+		System.out.println("更新一");
 	}
 
 }
