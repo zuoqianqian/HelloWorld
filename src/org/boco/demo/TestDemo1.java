44e39e1d4f6d1195649ef87ee8b40fa55a65df77
zuoqianqian <1577582192@qq.com>
2017-03-08 21:16:04 +0800

    新增类TestDemo1

---
+++ src/org/boco/demo/TestDemo1.java
@@ -0,0 +1,14 @@
+package org.boco.demo;
+
+public class TestDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c;
+		c = a + b;
+		System.out.println("c=" + c);
+	}
+
+}
